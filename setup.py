a46d2b1a5ef937717cf0955d5c3b9bb3304810d7
Vinicius Chiele <vinicius.chiele@gmail.com>
2015-05-09 13:05:23 +0100

    Added classifiers to the setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,20 @@ setup(
     description='A simple message bus for python 3',
     keywords=['simplebus', 'messagebus', 'message bus',
               'messaging', 'queue', 'topic', 'pubsub'],
-    install_requires=['simplejson>=3.6.5']
+    install_requires=['simplejson>=3.6.5'],
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Communications',
+          'Topic :: Internet',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Networking']
 )
