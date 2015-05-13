31ad6b5fa603a49dfc2006dd2f84cfedc861caea
viniciuschiele <vinicius.chiele@gmail.com>
2015-05-13 17:46:23 +0100

    Added new examples

---
+++ examples/disable_dead_letter.py
@@ -0,0 +1,30 @@
+# Copyright 2015 Vinicius Chiele. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from simplebus import Bus
+
+bus = Bus(app_id='disable_dead_letter')
+
+
+def on_message(message):
+    raise Exception('error')
+
+
+def consume():
+    bus.pull('simple_queue', on_message, dead_letter=False)
+
+
+if __name__ == '__main__':
+    consume()
+    bus.loop.start()  # block the current thread
