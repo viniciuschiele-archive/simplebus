8282bbb5a360bfdf1e26bd93dc78f64ae6d585ea
Vinicius Chiele <vinicius.chiele@gmail.com>
2015-09-07 22:16:33 +0100

    Removed old examples Added a new example

---
+++ examples/tasks.py
@@ -0,0 +1,29 @@
+from simplebus import SimpleBus
+
+bus = SimpleBus(app_id='tasks')
+
+
+@bus.command(address='tasks', concurrency=4, error_queue='tasks.error')
+class ConvertAudioCommand(object):
+    def __init__(self, audio_id):
+        self.audio_id = audio_id
+
+
+@bus.event(address='tasks.converted', concurrency=4, error_queue='tasks.converted.error')
+class AudioConvertedEvent(object):
+    def __init__(self, audio_id):
+        self.audio_id = audio_id
+
+
+@bus.handle(ConvertAudioCommand)
+def convert_audio(command):
+    bus.publish(AudioConvertedEvent(command.audio_id))
+
+
+@bus.handle(AudioConvertedEvent)
+def audio_converted(event):
+    print('Audio converted: ' + str(event.audio_id))
+
+if __name__ == '__main__':
+    bus.publish(ConvertAudioCommand(1))
+    bus.loop.start()
